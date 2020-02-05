63f63f0b37620b32daa9363954574408d9827bb6
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-05 08:02:37 +0100

    correcting test syntax error to pass until loop for bills

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -22,8 +22,8 @@ describe ATM do # Creating ATM class
             status: true, 
             message: 'success', 
             date: Date.today, 
-            amount: 45 
-            bills: [20, 20, 5]}
+            amount: 45, 
+            bills: [20, 20, 5] }
         expect(subject.withdraw(45, '1234', account)).to eq expected_output
     end # Testing that user sees correct output message "success + date + amount"
 
