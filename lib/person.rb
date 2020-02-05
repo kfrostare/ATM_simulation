fdab0134d64a77e949108295983c56a0549faad4
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-05 18:31:44 +0100

    pass test for missing name on person class

--- lib/person.rb
+++ lib/person.rb
@@ -24,11 +24,11 @@ class Person # Creating blueprints for the Person that uses the account
     end 
 
     def set_name(obj) # Declaring the attribute "name" as an object and setting it as name
-        obj = nil ? missing_name : @name = obj
+        obj == nil ? missing_name : @name = obj
     end
 
     def missing_name # Sending message if name is missing
-        raise ArgumentError, 'A person name is required'
+        raise 'A person name is required'
     end
 
     def set_owner(obj) # Declaring the attribute "owner" as object
@@ -36,7 +36,7 @@ class Person # Creating blueprints for the Person that uses the account
     end
 
     def missing_account # 
-        raise RuntimeError, 'No account present'
+        raise RuntimeError 'No account present'
     end
 
 end 
